42e76ecd80ef879528bf9aac3d5b4335f1bc8da6
Arnav <tijarearnav@gmail.com>
2023-11-25 23:33:34 +0530

    Add pattern code

---
+++ practicals/pattern.php
@@ -0,0 +1,18 @@
+<?php
+
+// Number of rows for the inverted triangle
+$numberOfRows = 5;
+
+// Loop through each row
+for ($currentRow = $numberOfRows; $currentRow >= 1; $currentRow--) {
+
+    // Print asterisks for the current row
+    for ($asteriskCount = 0; $asteriskCount <= $currentRow - 1; $asteriskCount++) {
+        echo "*";
+    }
+
+    // Move to the next line after completing the row
+    echo "\n";
+}
+
+?>
