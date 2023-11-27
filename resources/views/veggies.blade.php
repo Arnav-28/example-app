d7e0659cc8b4f31c62371ee8cf4700a8201df915
Arnav <tijarearnav@gmail.com>
2023-11-27 22:40:38 +0530

    Add routes for veggies

---
+++ resources/views/veggies.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sabzi Wallah</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #fcf8e3;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+
+        header {
+            background-color: #5bc0de;
+            color: #fff;
+            padding: 20px;
+            font-size: 36px;
+        }
+
+        table {
+            border-collapse: collapse;s
+            width: 60%;
+            margin: 20px auto;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 15px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #5bc0de;
+            color: #fff;
+            font-size: 18px;
+        }
+
+        td {
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1>SABZI LELOOOOO</h1>
+</header>
+
+<table>
+    <tr>
+        <th>Vegetable</th>
+        <th>Quantity</th>
+    </tr>
+    <tr>
+        <td style="background-color: #dff0d8;">Aaloo</td>
+        <td style="background-color: #dff0d8;">12</td>
+    </tr>
+    <tr>
+        <td style="background-color: #fcf8e3;">Gobhi</td>
+        <td style="background-color: #fcf8e3;">20</td>
+    </tr>
+    <tr>
+        <td style="background-color: #d9edf7;">Baigan</td>
+        <td style="background-color: #d9edf7;">30</td>
+    </tr>
+    <tr>
+        <td style="background-color: #f2dede;">Bhindi</td>
+        <td style="background-color: #f2dede;">40</td>
+    </tr>
+</table>
+</body>
+</html>
+
